46de597bf40180980dc3bac9f39108be67b33b05
Allan Pereira <allan.pereira@edu.itescia.fr>
2023-04-29 10:30:55 +0200

    Correction Login and tranfert data into mapScene, Add FoodPOI no clickable into the Map

--- Assets/Mapbox/Examples/Scripts/PoiLabelTextSetter.cs
+++ Assets/Mapbox/Examples/Scripts/PoiLabelTextSetter.cs
@@ -1,40 +1,49 @@
- namespace Mapbox.Examples
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace Mapbox.Examples
 {
-	using Mapbox.Unity.MeshGeneration.Interfaces;
-	using System.Collections.Generic;
-	using UnityEngine;
-	using UnityEngine.UI;
+    using Mapbox.Unity.MeshGeneration.Interfaces;
+    using System.Collections.Generic;
+    using UnityEngine.UI;
 
-	public class PoiLabelTextSetter : MonoBehaviour, IFeaturePropertySettable
-	{
-		[SerializeField]
-		Text _text;
-		[SerializeField]
-		Image _background;
+    public class PoiLabelTextSetter : MonoBehaviour, IFeaturePropertySettable, IPointerClickHandler
+    {
+        [SerializeField]
+        Text _text;
+        [SerializeField]
+        Image _background;
 
-		public void Set(Dictionary<string, object> props)
-		{
-			_text.text = "";
+        public void Set(Dictionary<string, object> props)
+        {
+			Debug.Log("JE SUIS UN TESTTTT");
+            _text.text = "";
 
-			if (props.ContainsKey("name"))
-			{
-				_text.text = props["name"].ToString();
-			}
-			else if (props.ContainsKey("house_num"))
-			{
-				_text.text = props["house_num"].ToString();
-			}
-			else if (props.ContainsKey("type"))
-			{
-				_text.text = props["type"].ToString();
-			}
-			RefreshBackground();
-		}
+            if (props.ContainsKey("name"))
+            {
+                _text.text = props["name"].ToString();
+            }
+            else if (props.ContainsKey("house_num"))
+            {
+                _text.text = props["house_num"].ToString();
+            }
+            else if (props.ContainsKey("type"))
+            {
+                _text.text = props["type"].ToString();
+            }
+            RefreshBackground();
+        }
 
-		public void RefreshBackground()
-		{
-			RectTransform backgroundRect = _background.GetComponent<RectTransform>();
-			LayoutRebuilder.ForceRebuildLayoutImmediate(backgroundRect);
-		}
-	}
+        public void RefreshBackground()
+        {
+            RectTransform backgroundRect = _background.GetComponent<RectTransform>();
+            LayoutRebuilder.ForceRebuildLayoutImmediate(backgroundRect);
+        }
+
+        public void OnPointerClick(PointerEventData eventData)
+        {
+            // Ajouter ici le code pour gérer le clic sur le texte du POI, par exemple pour ouvrir une fenêtre contextuelle avec des informations supplémentaires sur le POI.
+            Debug.Log("Clic sur le POI : " + _text.text);
+        }
+    }
 }
--- Assets/Script/Controler/StatsMapController.cs
+++ Assets/Script/Controler/StatsMapController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using TMPro;
 
 public class StatsMapController : MonoBehaviour
@@ -10,17 +11,40 @@ public class StatsMapController : MonoBehaviour
     [SerializeField] private TextMeshProUGUI expText;
     [SerializeField] private TextMeshProUGUI goldText;
     [SerializeField] private TextMeshProUGUI pgoldText;
-    [SerializeField] private TextMeshProUGUI avatarText;
+    [SerializeField] private RawImage avatarImg;
+    [SerializeField] private Texture orcTexture;
+    [SerializeField] private Texture elfTexture;
+    [SerializeField] private Texture undeadTexture;
+
+    public static UserLogged userLogged = new UserLogged();
     // Start is called before the first frame update
     void Start()
 {
+    if (LoginScript.userLogged.id != 0)
+        {
+            userLogged = LoginScript.userLogged;
+        }else
+        {
+            userLogged = CreateStatScript.userLogged;
+        }
     //Change the text by the UserLogged data in LoginScript
-    nameText.text = LoginScript.userLogged.name;
-    lvlText.text = LoginScript.userLogged.stats.ID_Level.ToString();
-    expText.text = LoginScript.userLogged.stats.Exp.ToString();
-    goldText.text = LoginScript.userLogged.stats.Gold.ToString();
-    pgoldText.text = LoginScript.userLogged.stats.Premium_gold.ToString();
-    avatarText.text = LoginScript.userLogged.stats.Race;
+    nameText.text = userLogged.name;
+    lvlText.text = userLogged.stats.ID_Level.ToString();
+    expText.text = userLogged.stats.Exp.ToString();
+    goldText.text = userLogged.stats.Gold.ToString();
+    pgoldText.text = userLogged.stats.Premium_gold.ToString();
+    if (userLogged.stats.Race == "Orc")
+    {
+        avatarImg.texture = orcTexture;
+    }else if (userLogged.stats.Race == "Elf")
+    {
+        avatarImg.texture = elfTexture;
+    }else
+    {
+        avatarImg.texture = undeadTexture;
+    }
+    //userLogged.stats.Race
+    // avatarText.text = testImg;
 
 }
     // Update is called once per frame
